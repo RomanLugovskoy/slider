fbcae2f1418efefd94ee4e0aa494a4a950786168
RomanLugovskoy <romagma29@gmail.com>
2024-05-18 03:58:34 +0300

    add func on arr

--- js/script.js
+++ js/script.js
@@ -1,17 +1,64 @@
 const slides = [
 	{
-
-	}
+		city: 'Rostov-on-Don<br>LCD admiral',
+        area: '81 m<sup>2</sup>',
+        time: '3.5 months',
+        cost: 'Upon request',
+        img: 'img/admiral.jpg',
+	},
+	{
+		city: 'Sochi<br>Thieves',
+        area: '105 m<sup>2</sup>',
+        time: '4 months',
+        cost: 'Upon request',
+        img: 'img/sochi.jpg',
+	},
+	{
+		city: 'Rostov-on-Don<br>Patriotic',
+        area: '93 m<sup>2</sup>',
+        time: '3 months',
+        cost: 'Upon request',
+        img: 'img/patriotic.jpg',
+	},
 ];
+
+const headCityName = document.querySelectorAll('.slider-head__item-link');
+
+const cityElem = document.querySelector('.city-info');
+const areaElem = document.querySelector('.area-info');
+const timeElem = document.querySelector('.time-info');
+const costElem = document.querySelector('.cost-info');
+
+const imgElem = document.querySelector('.slider-main__img');
+
+const dots = document.querySelectorAll('.controlls-dot');
 
 const prev = document.querySelector('.controlls__prev');
 const next = document.querySelector('.controlls__next');
-const img = document.querySelector('.slider-main__img');
 
-const dottsArray = document.querySelectorAll('.controlls-dot');
-console.log(dottsArray);
+let currentIndex = 0;
+const changeSlide = (index) => {
+	if (index >= 0 && index <= slides.length - 1) {
+		cityElem.innerHTML = slides[index].city;
+		areaElem.innerHTML = slides[index].area;
+		timeElem.innerHTML = slides[index].time;
+		costElem.innerHTML = slides[index].cost;
+		imgElem.src = `${slides[index].img}`;
+		
+	} else if (index < 0) {
+		currentIndex = slides.length - 1;
+		changeSlide(currentIndex);
+	} else if (index >= slides.length) {
+		currentIndex = 0;
+		changeSlide(currentIndex);
+	}
+};
 
-next.addEventListener('click', function() {
-	dottsArray[1].classList.add('dot_active')
-	img.src = 'img/patriotic.jpg';
+prev.addEventListener('click', function () {
+	currentIndex -= 1;
+	changeSlide(currentIndex);
+});
+next.addEventListener('click', function () {
+	currentIndex += 1;
+	changeSlide(currentIndex);
 });
